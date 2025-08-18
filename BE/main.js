08088676d61e64c1ab9e9ca75b3e8b4876ce9fe0
aradhyacp <cparadhya@proton.me>
2025-08-18 22:21:41 +0530

    backend added multiple model support

--- BE/main.js
+++ BE/main.js
@@ -20,12 +20,12 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/ai-summarizer", async (req, res) => {
-  const { language, codeSent } = req.body;
+app.post("/ai-summary", async (req, res) => {
+  const { language, codeSent, model } = req.body;
 
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-pro",
+      model: model,
       contents: `You are an AI code summarizer/explainer. Your job is to analyze and summarize code snippets, functions. Provide clear, concise, and accurate explanations of what the code does, focusing on the logic and purpose. You will go through each function and explain it. Avoid guessing beyond what's in the code. Use plain language that developers of all levels can understand. When relevant, mention input/output, dependencies, and overall structure. Do not rewrite the code. Do not speculate or over-explain. Response only if the user provides with code, ignore if the user provide other than code. Go through the code given below and Summarize the following code written in ${language}
       code: ${codeSent}`,
     });
