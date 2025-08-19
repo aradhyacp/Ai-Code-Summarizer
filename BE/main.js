658f0b6477e092590a2f0b5e329d85def5b85f1f
aradhyacp <cparadhya@proton.me>
2025-08-19 18:57:10 +0530

    backend production test

--- BE/main.js
+++ BE/main.js
@@ -6,7 +6,7 @@ import { z } from "zod";
 import rateLimit from "express-rate-limit";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 dotenv.config();
 app.use(express.json());
 app.use(cors());
@@ -87,6 +87,6 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.listen(port, () => {
+app.listen(port,"0.0.0.0", () => {
   console.log(`Server running on http://localhost:${port}`);
 });
