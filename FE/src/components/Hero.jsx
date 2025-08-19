9acb427c6f65e20789f292b5a7a91fe9d521d974
aradhyacp <cparadhya@proton.me>
2025-08-19 19:06:47 +0530

    added production backend url

--- FE/src/components/Hero.jsx
+++ FE/src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
     setAiSummary("");
 
     try {
-      const res = await fetch("http://localhost:3000/ai-summary", {
+      const res = await fetch("https://ai-code-summarizer.onrender.com/ai-summary", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
